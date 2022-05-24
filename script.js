00edb79054c6ea94d2c84fca7fec5ccdb0865cd6
Bradley Beard <Brad1012@hotmail.co.uk>
2022-05-24 17:38:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+  function getRandomItem(myArray) {
+  const randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+
+  const item = myArray[randomItem];
+  return item;
+  }
+
+  const array = [
+    "Apples",
+    "Bananas",
+    "Pears"
+  ];
+
+const result = getRandomItem(array);
+console.log(result);
