bfe1eb6e7f240d8c07f40975ef4152e9ece67824
Bradley Beard <Brad1012@hotmail.co.uk>
2022-06-20 16:06:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ function GetValue()
     "Frag montage time, kill a pirate with a 360 EoR shot!",
     "You can only move by using sword hops for 30 seconds.",
     );
+    
+    /*
+    Hard - Easy - PVP - Long - Individual/Crew Based - 
+    "Put a piece of loot onto another players ship.",
+    "Frag montage time, kill a pirate with a 360 EoR shot!",
+    "Double gun time! Use a Pistol and Eye of Reach until next you die.",
+    "Dam Spaniards are piping up again! Go clear a Sea Fort.",
+    "Poseidon is getting annoyed, calm him down by throwing 1 piece of loot into the sea.",
+    "Ring the crows nest bell on another players ship!"
+    */
     var random = myarray[Math.floor(Math.random() * myarray.length)];
    document.getElementById("task-text").innerHTML=random;
 }
